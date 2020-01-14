91818a8b92318795c8a59d73ade6eddbf14bdaf9
Eian Carter <eian@bricleir.com>
2020-01-14 09:00:24 -0800

    started from scratch to save the state in one place

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,17 +1,37 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+// // your components will all go in this `component` directory.
+// // feel free to change this component.js into TodoList.js
+// import React from 'react';
+// import Todo from './Todo';
+
+
+// const TodoList = props => {
+//         return (
+//             <div>
+//                 {props.todos.map(item => (
+//                     <Todo key={item.id} item={item} toggleItem={props.toggleItem} />
+//                 ))}
+//             </div>
+//         );
+// };
+
+// export default TodoList;
+
 import React from 'react';
+
 import Todo from './Todo';
 
-
 const TodoList = props => {
-        return (
-            <div>
-                {props.todos.map(item => (
-                    <Todo key={item.id} item={item} toggleItem={props.toggleItem} />
-                ))}
-            </div>
-        );
+  return (
+    <div>
+      {props.todos.map(todo => (
+        <Todo
+          handleToggleComplete={props.handleToggleComplete}
+          key={todo.id}
+          todo={todo}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
